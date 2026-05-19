0dc3499370e5aa085417baf0d7f87dc98a42078b
amerindriskcontrol-beep <amerindriskcontrol@gmail.com>
2026-05-19 14:29:18 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/* Basic pre-cache service worker */
+const CACHE = 'home-ready-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.webmanifest',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(net => {
+      const copy = net.clone();
+      caches.open(CACHE).then(cache => cache.put(e.request, copy));
+      return net;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
